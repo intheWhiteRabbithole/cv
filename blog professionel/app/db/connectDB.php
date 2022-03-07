5625387f35807857a83734aec97c9e89100f73db
Alex Dr <97302651+intheWhiteRabbithole@users.noreply.github.com>
2022-03-07 15:15:09 +0100

    Add files via upload

---
+++ blog professionel/app/db/connectDB.php
@@ -0,0 +1,32 @@
+<?php
+
+$host = 'localhost';
+$db = 'blog_db';
+$psw = 'root';
+$port = '3307';
+$user = 'root';
+$charset = 'utf8';
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+
+$options = [
+   \PDO::ATTR_ERRMODE    =>   \PDO::ERRMODE_EXCEPTION,
+   \PDO::ATTR_DEFAULT_FETCH_MODE     =>  \PDO::FETCH_ASSOC,
+   \PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+try {
+    $pdo = new PDO($dsn,$user,$psw,$options);
+    echo 'connection etablished ';
+
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getCode(),$e->getMessage());
+}
+
+
+
+
+
+
+
+
+?>
---
+++ blog professionel/public/footer.php
@@ -0,0 +1,12 @@
+
+<footer>
+
+        <a class="element-navbar" href="https://www.linkedin.com/">LinkedIn</a>
+        <a class="element-navbar" href="https://twitter.com/">Twitter</a>
+        <a class="element-navbar" href="https://www.appbrewery.co/">Website</a>
+        <p class="adresse-mail">© Name Surname.</p>
+
+</footer>
+</body>
+
+</html>
